0e7a68597a01fbe8e33db11941493187d6ee0655
Solanki <solankijinali210@gmail.com>
2025-02-16 18:11:31 +0530

    Laravel maintenance demo

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>We'll be back soon!</title>
+    <style>
+        body { text-align: center; padding: 50px; font-family: Arial, sans-serif; }
+        h1 { font-size: 50px; }
+        p { font-size: 20px; color: #666; }
+    </style>
+</head>
+<body>
+    <h1>We'll be back soon!</h1>
+    <p>Sorry for the inconvenience, we're performing some maintenance at the moment.</p>
+    <p> We'll be back shortly!</p>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LARAVEL DEMO</title>
+    <style>
+        body { text-align: center; padding: 50px; font-family: Arial, sans-serif; }
+        h1 { font-size: 50px; }
+        p { font-size: 20px; color: #666; }
+    </style>
+</head>
+<body>
+    <h1>WELECOME TO LARAVEL DEMO PROJECT</h1>
+</body>
+</html>
